50cd01313c154b66bf2af3848db5ba6519bf9b58
srlogs <logeshwaran.sr1@gmail.com>
2020-12-21 02:00:40 +0530

    hidden environment data

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -6,14 +6,7 @@ export const environment = {
   production: false,
   // For Firebase JS SDK v7.20.0 and later, measurementId is optional
   firebaseConfig: {
-    apiKey: 'AIzaSyCFVfReist9rZ_zpiQGorNPYDTcy2E0DIE',
-    authDomain: 'fir-test-439cd.firebaseapp.com',
-    databaseURL: 'https://fir-test-439cd-default-rtdb.firebaseio.com',
-    projectId: 'fir-test-439cd',
-    storageBucket: 'fir-test-439cd.appspot.com',
-    messagingSenderId: '494492730054',
-    appId: '1:494492730054:web:8dcfbb41fe6488e1118dc7',
-    measurementId: 'G-GVDLV5NL4X',
+    // Enter your config file ra beta
   },
 };
 
